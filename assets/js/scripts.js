211b0daa697d01171cd2ba1276e526c5a8b7798e
Anaïs Biossat <73831862+B-Bioss@users.noreply.github.com>
2020-11-23 10:23:48 +0100

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+// Pour Changer le curseur de la souris
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+
+})
